415ec5556b4bcf2a80bf9fd3f733bc63301cab5b
mathew <amonline2005@gmail.com>
2025-12-09 22:17:48 +0530

    scrolling made smoother

--- task1/app/components/Chatroom.tsx
+++ task1/app/components/Chatroom.tsx
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Chatmessage from './Chatmessage'
 
 const socket = io('http://localhost:5000')
@@ -8,6 +8,11 @@ export default function Chatroom() {
   const [messages, setMessages] = useState<{ who: string; message: string }[]>(
     []
   )
+  const bottomRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }, [messages])
 
   useEffect(() => {
     socket.on('recieve_msg', (data) => {
@@ -16,7 +21,7 @@ export default function Chatroom() {
   }, [socket])
 
   return (
-    <div className='flex flex-col space-y-2 p-4'>
+    <div className='flex flex-col bg-teal-500 space-y-2 p-4' ref={bottomRef}>
       {messages.map((msg, index) => (
         <Chatmessage key={index} who={msg.who} message={msg.message} />
       ))}
